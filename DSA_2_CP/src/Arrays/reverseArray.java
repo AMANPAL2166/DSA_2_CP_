ca45adb3069bc6f55164fc9dfc09b5667baaab18
Aman Pal <apal60525@gmail.com>
2025-07-24 12:56:20 +0530

    change reverse file

--- DSA_2_CP/src/Arrays/reverseArray.java
+++ DSA_2_CP/src/Arrays/reverseArray.java
@@ -2,6 +2,7 @@ package Arrays;
 
 public class reverseArray {
     public static void main(String[] args) {
-        System.out.println("Aman");
+
+        System.out.println("Aman tu chutiya hai ky");
     }
 }
