723e73a40641907ac22c220d08fa5597cd782945
Aman Pal <apal60525@gmail.com>
2025-07-29 00:32:30 +0530

    Create a Cyclesort_01 file for our new algo.. (CycleSort)

--- DSA_2_CP/src/Sorting/Cyclesort_01.java
+++ DSA_2_CP/src/Sorting/Cyclesort_01.java
@@ -1,9 +1,43 @@
 //Given an array arr[] of size n-1 with distinct integers in the range of [1, n]. This array represents a permutation of the integers from 1 to n with one element missing. Find the missing element in the array.
 //https://www.geeksforgeeks.org/dsa/find-the-missing-number/
 package Sorting;
+//First explain Cycle_sort:- Cycle sort is all about for those question whose range between 0-n. It's better time & space compalaxity.
 
+//Given an array arr[] of size n-1 with distinct integers in the range of [1, n]. This array represents a permutation of the integers from 1 to n with one element missing. Find the missing element in the array.
+//Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+//Output: 6
+//Explanation: All the numbers from 1 to 8 are present except 6.
+
+//Input: arr[] = [1, 2, 3, 5]
+//Output: 4
+//Explanation: Here the size of the array is 4, so the range will be [1, 5]. The missing number between 1 to 5 is 4
+
+
+//-----/::) MOre code will upload on 29-08-2025
 public class Cyclesort_01 {
     public static void main(String[] args) {
 
     }
+    static void  missingNum(int[]arr){
+        int n = arr.length;
+        int i = 0;
+        while (i<n){
+            for(int j = 0;j<n; j++){
+                int correct = arr[j];
+                if(arr[j] != arr[i]){
+                    swap(int[] arr, i, correct)
+                } else if () {
+                    
+                }
+
+            }
+        }
+
+    }
+    static void swap(int[] arr, int i , int correct){
+        int temp = arr[i];
+        arr[i] = arr[correct];
+        arr[correct] = temp;
+    }
+
 }
