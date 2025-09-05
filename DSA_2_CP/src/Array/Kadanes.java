d6814f88c5374c9ff2dcca126a15c03289d7c3c1
Aman Pal <apal60525@gmail.com>
2025-09-05 10:52:28 +0530

    Update Kandanes

--- DSA_2_CP/src/Array/Kadanes.java
+++ DSA_2_CP/src/Array/Kadanes.java
@@ -31,4 +31,17 @@ public class Kadanes {
         return maxsum;
 
     }
+    public int max_sumarray(int[] nums){
+        int n = nums.length;
+        int currsum =0;
+        int maxsum = Integer.MIN_VALUE;
+        for(int i = 0; i<n; i++){
+            currsum += nums[i];
+            maxsum = Math.max(maxsum, currsum);
+            if(currsum<0){
+                currsum = 0;
+            }
+        }
+        return maxsum;
+    }
 }
