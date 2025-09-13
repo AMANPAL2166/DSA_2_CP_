6f5fe87caf36851d8a6660a8e1f05ca4945ea23a
Aman Pal <apal60525@gmail.com>
2025-09-13 23:25:34 +0530

    13Sep

--- DSA_2_CP/src/Array/Merge_sort.java
+++ DSA_2_CP/src/Array/Merge_sort.java
@@ -2,32 +2,40 @@ package Array;
 
 public class Merge_sort {
     public static void main(String[] args) {
+        int[] num = {3, 5, 3, 6};
+        int m = 1;
+        int[] nums1 = {3, 5, 4, 2, 6};
+        int n = 2;
+        Merge_sort obj = new Merge_sort();
+        obj.merge(num, m, nums1, n);
 
 
     }
-    public   void merge(int[] nums1, int m, int[] nums2, int n) {
 
-    int i = m - 1; // last element of nums1
-    int j = n - 1; // last element of nums2
-    int k = m + n - 1; // last position in nums1
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1; // last element of nums1
+        int j = n - 1; // last element of nums2
+        int k = m + n - 1; // last position in nums1
 
         while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // if nums2 still has elements
+        while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
+            k--;
         }
-        k--;
-    }
 
-    // if nums2 still has elements
-        while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
+
     }
-}
-}
 }
