2c5d35256af2e7813cdffe61dc9c47f1ea779706
Aman Pal <apal60525@gmail.com>
2025-09-14 23:12:13 +0530

    14Sep

--- DSA_2_CP/src/Searching/Single_element.java
+++ DSA_2_CP/src/Searching/Single_element.java
@@ -2,14 +2,14 @@ package Searching;
 
 public class Single_element {
     public static void main(String[] args) {
-        int[] nums = {1};
-        System.out.println(singleNonDuplicate1(nums));
+        int[] nums = {1,1,2};
+        System.out.println(singleNonDuplicate2(nums));
 
     }
 
     public static int singleNonDuplicate1(int[] nums) {// using linear search
         int n = nums.length;
-        if (n < 1) {
+        if (n == 1) {
             return nums[0];
         }
         for (int i = 1; i < n - 1; i++) {
@@ -22,13 +22,40 @@ public class Single_element {
         return nums[n - 1];
     }
 
-    public static int singleNonDuplicate2(int[] nums) {
+    public static int singleNonDuplicate2(int[] nums) {  ///  This function is not right. I'll update after some times
         // using Binary search
         int n = nums.length;
-        if(n<1){
+        if(n==1){
             return nums[0];
+        }else if(n==2){
+            if(nums[0] == nums[1]){
+                return nums[2];
+            }
         }
-        int st = 0, end = n-1;
+        int st = 1, end = n-1;
+        while(st<=end){
+            int mid = st+(end - st)/2;
+            if(nums[mid ] != nums[mid-1]  && nums[mid]!= nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid%2==0){//even case
+                if(nums[mid ] == nums[mid-1]){
+                    end = mid-1;
+                }else{
+                    st = mid+1;
+                }
+
+            }else{
+                if(nums[mid ] == nums[mid-1]){
+                    st = mid+1;
+
+                }else{
+                    end = mid-1;
+                }
+
+            }
+        }
+        return -1;
 
     }
 
