d908b9d9702e58fb49ac31dcfb368ba3c7e18a04
Aman Pal <apal60525@gmail.com>
2025-09-14 14:32:09 +0530

    14Sep

--- DSA_2_CP/src/Searching/Single_element.java
+++ DSA_2_CP/src/Searching/Single_element.java
@@ -2,6 +2,34 @@ package Searching;
 
 public class Single_element {
     public static void main(String[] args) {
+        int[] nums = {1};
+        System.out.println(singleNonDuplicate1(nums));
 
     }
+
+    public static int singleNonDuplicate1(int[] nums) {// using linear search
+        int n = nums.length;
+        if (n < 1) {
+            return nums[0];
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] == nums[i - 1]) {
+                i++;
+            } else {
+                return nums[i - 1];
+            }
+        }
+        return nums[n - 1];
+    }
+
+    public static int singleNonDuplicate2(int[] nums) {
+        // using Binary search
+        int n = nums.length;
+        if(n<1){
+            return nums[0];
+        }
+        int st = 0, end = n-1;
+
+    }
+
 }
