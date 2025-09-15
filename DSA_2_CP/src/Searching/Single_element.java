0dda9fd4e5fd680bb141851e85c7391649a58f25
Aman Pal <apal60525@gmail.com>
2025-09-15 14:12:44 +0530

    14Sep

--- DSA_2_CP/src/Searching/Single_element.java
+++ DSA_2_CP/src/Searching/Single_element.java
@@ -2,7 +2,7 @@ package Searching;
 
 public class Single_element {
     public static void main(String[] args) {
-        int[] nums = {1,1,2};
+        int[] nums = {1,2,2,3,3};
         System.out.println(singleNonDuplicate2(nums));
 
     }
@@ -22,41 +22,41 @@ public class Single_element {
         return nums[n - 1];
     }
 
-    public static int singleNonDuplicate2(int[] nums) {  ///  This function is not right. I'll update after some times
-        // using Binary search
+    public static int singleNonDuplicate2(int[] nums) {
         int n = nums.length;
-        if(n==1){
+        if(  n == 1){
             return nums[0];
-        }else if(n==2){
+        }
+        if(n == 3){
             if(nums[0] == nums[1]){
                 return nums[2];
+            }else{
+                return nums[0];
             }
         }
         int st = 1, end = n-1;
         while(st<=end){
-            int mid = st+(end - st)/2;
-            if(nums[mid ] != nums[mid-1]  && nums[mid]!= nums[mid+1]){
+            int mid = st+(end-st)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
                 return nums[mid];
             }
-            if(mid%2==0){//even case
-                if(nums[mid ] == nums[mid-1]){
+            if(mid%2==0){
+                //even check
+                if(nums[mid] == nums[mid-1]){
                     end = mid-1;
                 }else{
-                    st = mid+1;
+                    st = end+1;
                 }
 
             }else{
-                if(nums[mid ] == nums[mid-1]){
+                if(nums[mid] == nums[mid-1]){
                     st = mid+1;
-
                 }else{
                     end = mid-1;
                 }
-
             }
+
         }
-        return -1;
-
+        return nums[n-1];
     }
-
 }
