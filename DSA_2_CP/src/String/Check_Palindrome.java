13e01ccf7e8e9208be133d165f2d4b9254d2bcee
Aman Pal <apal60525@gmail.com>
2025-09-17 22:21:05 +0530

    17Sep

--- DSA_2_CP/src/String/Check_Palindrome.java
+++ DSA_2_CP/src/String/Check_Palindrome.java
@@ -1,12 +1,16 @@
 package String;
+
+//https://leetcode.com/problems/valid-palindrome/submissions/1774106600/
+
 //https://www.geeksforgeeks.org/dsa/palindrome-string/
 public class Check_Palindrome {
     public static void main(String[] args) {
-        String s = "abc";
-        System.out.println(palindrome(s));
+        String s = "0P";
+        System.out.println(palindrome2(s));
     }
 
      static boolean palindrome(String s) {
+        
         int left = 0;
         int right = s.length()-1;
 
@@ -18,6 +22,38 @@ public class Check_Palindrome {
                 return false;
             left ++;
             right--;
+        }
+        return true;
+    }
+    static boolean palindrome2(String s ) {
+        //Base case
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            // Skip non-alphanumeric characters from the left
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right
+            else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            }
+            // If both characters are alphanumeric, compare them
+            else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
         }
         return true;
     }
