d75a3c35ba92ebd670032abefb2b8c49b0540302
Aman Pal <apal60525@gmail.com>
2025-09-21 12:30:03 +0530

    21Sep

--- DSA_2_CP/src/String/Camel_match.java
+++ DSA_2_CP/src/String/Camel_match.java
@@ -1,4 +1,37 @@
 package String;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Camel_match {
+    public static void main(String[] args) {
+
+    }
+    public List<Boolean> camelMathc (String[] queries,String pattern){
+        List<Boolean> result = new ArrayList<>();
+        for (String q : queries) {
+            result.add(isMatch(q, pattern));
+        }
+        return result;
+    }
+
+    private boolean isMatch(String query, String pattern) {
+        int i = 0, j = 0; // i for query, j for pattern
+        while (i < query.length()) {
+            char qc = query.charAt(i);
+
+            if (j < pattern.length() && qc == pattern.charAt(j)) {
+                // match char in both
+                i++;
+                j++;
+            } else {
+                if (Character.isUpperCase(qc)) {
+                    // extra uppercase → not allowed
+                    return false;
+                }
+                i++; // skip lowercase
+            }
+        }
+        return j == pattern.length(); // pattern fully matched
+    }
 }
