5cd1d10144c9463bdd2fdddf9104b4d18a247389
Aman Pal <apal60525@gmail.com>
2025-09-22 14:12:43 +0530

    22Sep

--- DSA_2_CP/src/Recurssion/factorial.java
+++ DSA_2_CP/src/Recurssion/factorial.java
@@ -1,4 +1,19 @@
 package Recurssion;
 
 public class factorial {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("factorial:- "+factorial(n));
+
+
+    }
+    public static int factorial(int n){
+        //base case
+        if(n<=1){
+            return 1;
+
+        }
+        return n*factorial(n-1);
+
+    }
 }
