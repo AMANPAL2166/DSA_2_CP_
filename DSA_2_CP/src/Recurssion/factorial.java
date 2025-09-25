4675bf64cd1918aa4a0aa9725d9a9a0483740d97
Aman Pal <apal60525@gmail.com>
2025-09-25 13:02:57 +0530

    "Solve factorial using recursion "

--- DSA_2_CP/src/Recurssion/factorial.java
+++ DSA_2_CP/src/Recurssion/factorial.java
@@ -3,7 +3,7 @@ package Recurssion;
 public class factorial {
     public static void main(String[] args) {
         int n = 4;
-        System.out.println("factorial:- "+factorial(n));
+        System.out.println("factorial:- "+fact(n));
 
 
     }
@@ -15,5 +15,14 @@ public class factorial {
         }
         return n*factorial(n-1);
 
+    }
+    public static int fact(int n ){
+        //base case
+        if(n<=1){
+            return 1;
+        }
+        int fac = fact(n-1);
+        int fac1 = n*fac;
+        return fac1;
     }
 }
