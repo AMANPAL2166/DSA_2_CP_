3f87d772ad9f9e7167d7c6aea8a4e0b4b0294524
Aman Pal <apal60525@gmail.com>
2025-09-26 14:35:26 +0530

    "Solve CalculatePower using recursion(Log) "

--- DSA_2_CP/src/Recurssion/CalculatePower.java
+++ DSA_2_CP/src/Recurssion/CalculatePower.java
@@ -2,8 +2,8 @@ package Recurssion;
 
 public class CalculatePower {
     public static void main(String[] args) {
-        int n = 2, s =-2;
-        System.out.println(cal(n,3));
+        int n = 10, s =-2;
+        System.out.println(cal1(n, 8));
 
     }
 
@@ -18,4 +18,21 @@ public class CalculatePower {
         return xn;
 
     }
+
+    public static double cal1(int x, int n) {{
+        //base case
+        if(n == 0){
+            return 1;
+        }
+        double xpn2 = cal1(x, n/2);
+        double xn = xpn2*xpn2;
+        //there are if case
+        if(n%2 == 1){
+            xn = xn*x;
+        }
+        return xn;
+    }
+
+    }
+
 }
