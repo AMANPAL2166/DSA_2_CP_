1e2d44dd3a58b321c16e3a1cc2f8c576b61d2a81
Aman Pal <apal60525@gmail.com>
2025-09-29 20:22:27 +0530

    "Solving unique occurance "

---
+++ DSA_2_CP/src/Array/Unique.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class Unique {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean uniqueOccurrences(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+        for(int i = 0; i<n;i++  ){
+            for(int j =1; j<n;j++){
+                if(arr[i] == arr[j]){
+                    ;
+
+                }
+            }
+        }
+
+
+
+        return true;
+
+
+    }
+}
