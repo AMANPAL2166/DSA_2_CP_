bc581316cef6a118f724122c32693f1907d8a992
Aman Pal <apal60525@gmail.com>
2025-10-11 19:32:07 +0530

    "Java basic -Print digit"

--- DSA_2_CP/src/Fundamental/count_digit.java
+++ DSA_2_CP/src/Fundamental/count_digit.java
@@ -5,12 +5,38 @@ import java.util.Scanner;
 public class count_digit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int no = sc.nextInt();
-        int count = 0;
-        while(no != 0){
-            no = no/10;
-            count++;
+        int no = 2342;
+//        int count = 0;
+//        while(no != 0){
+//            no = no/10;
+//            count++;
+//        }
+//        System.out.println(count);
+        printno(no);
+
+    }
+
+    public static void printno(int n) {
+//        while(n!=0){
+//            int rem = n%10;
+//            System.out.println(rem);
+//            n = n/10;
+//        }
+        //Method second
+        int nod = 0;
+        int temp = n;
+        while(temp != 0){
+            temp =temp/10;
+            nod++;
         }
-        System.out.println(count);
+        int div = (int)Math.pow(10, nod-1);
+        while(n!=0){
+            int q = n/div;
+            System.out.println(q);
+            n= n%div;
+            div = div/10;
+
+        }
     }
 }
+
