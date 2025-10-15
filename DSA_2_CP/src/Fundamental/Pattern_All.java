7d82bbf668903cb1bfde7c4321a803669417c42e
Aman Pal <apal60525@gmail.com>
2025-10-15 18:29:20 +0530

    "Java basic - Pattern 4th"

--- DSA_2_CP/src/Fundamental/Pattern_All.java
+++ DSA_2_CP/src/Fundamental/Pattern_All.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Pattern_All {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in) ;
-        int n  = sc.nextInt();
+        int n  = 5;
 //        first(n);
 //        second(n);
-        third(n);
+//        third(n);
+        fourth(n);
 
 
     }
@@ -36,13 +37,28 @@ public class Pattern_All {
         int st = 1;
         for(int i  = 1; i<=n; i++){
             for(int j = 1; j<=sp; j++){
-                System.out.print ("\t ");
+                System.out.print ("\t");
             }
             for(int k = 1; k<=st; k++){
                 System.out.print("*\t");
             }
             sp--;
             st++;
+            System.out.println();
+        }
+    }
+    public static void fourth(int n) {
+        int sp = 0 ;
+        int st = n;
+        for(int i  = 1; i<=n; i++){
+            for(int j = 1; j<=sp; j++){
+                System.out.print ("\t");
+            }
+            for(int k = 1; k<=st; k++){
+                System.out.print("*\t");
+            }
+            sp++;
+            st--;
             System.out.println();
         }
     }
