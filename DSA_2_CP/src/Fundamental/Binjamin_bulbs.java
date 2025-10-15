b4a90131b27e872ac6571a941854cfaf52017901
Aman Pal <apal60525@gmail.com>
2025-10-15 13:19:42 +0530

    "Java basic -Binjamin Puzzle"

---
+++ DSA_2_CP/src/Fundamental/Binjamin_bulbs.java
@@ -0,0 +1,10 @@
+package Fundamental;
+
+public class Binjamin_bulbs {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i*i<=n;i++){
+            System.out.println(i*i);
+        }
+    }
+}
