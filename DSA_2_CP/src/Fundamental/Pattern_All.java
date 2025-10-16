0596366e478012d89442442c5f9401b8fa637128
Aman Pal <apal60525@gmail.com>
2025-10-16 11:56:58 +0530

    "Java basic - Pattern 6th"

--- DSA_2_CP/src/Fundamental/Pattern_All.java
+++ DSA_2_CP/src/Fundamental/Pattern_All.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Pattern_All {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in) ;
-        int n  = 5;
+        int n  = 7;
 //        first(n);
 //        second(n);
 //        third(n);
 //        fourth(n);
-        fifth(n);
+//        fifth(n);
+        sixth(n);
 
 
     }
@@ -84,5 +85,31 @@ public class Pattern_All {
              }
              System.out.println(" ");
          }
+    }
+
+    public static void sixth(int n) {
+
+        int sp = 1;
+        int st = n/2+1 ;
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=st;j++){
+                System.out.print ("*\t");
+
+            }
+            for (int j = 1; j<=sp; j++){
+                System.out.print ("\t");
+            }
+            for(int j = 1;j<=st; j++){
+                System.out.print ("*\t");
+            }
+            if(i<=n/2){
+                sp+=2;
+                st--;
+            }else{
+                sp-=2;
+                st ++;
+            }
+            System.out.println(" ");
+        }
     }
 }
