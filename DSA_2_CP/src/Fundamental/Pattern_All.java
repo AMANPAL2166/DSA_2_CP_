82fb029473f864fadb62e9edb4673cadbe6569a4
Aman Pal <apal60525@gmail.com>
2025-10-16 20:04:36 +0530

    "Java basic - Pattern 10th"

--- DSA_2_CP/src/Fundamental/Pattern_All.java
+++ DSA_2_CP/src/Fundamental/Pattern_All.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Pattern_All {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in) ;
-        int n  = 7;
+        int n  = 5;
 //        first(n);
 //        second(n);
 //        third(n);
@@ -15,7 +15,8 @@ public class Pattern_All {
 //        seventh(n);
 //        eighth(n);
 //        eight_2(n);
-        nineth(n);
+//        nineth(n);
+        tenth(n);
 
 
 
@@ -170,12 +171,36 @@ public class Pattern_All {
                     System.out.print ("\t");
 
                 }
-
             }
             System.out.println(" ");
 
 
         }
 
+    }
+
+    public static void tenth(int n) {
+        int os = n/2;
+        int is  = -1;
+        for(int i =1; i<=n; i++){
+            for(int j =1; j<=os;j++){
+                System.out.print ("\t");
+            }
+            System.out.print ("*\t");
+            for(int j =1; j<=is;j++){
+                System.out.print ("\t");
+            }
+            if(i>1 && i<n){
+                System.out.print ("*\t");
+            }
+            if(i<=n/2){
+                os--;
+                is +=2;
+            }else{
+                os++;
+                is -=2;
+            }
+            System.out.println(" ");
+        }
     }
 }
