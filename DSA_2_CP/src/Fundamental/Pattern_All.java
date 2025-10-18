bbc6df425e47f6bd3335e8329c9989a2c56ce2c6
Aman Pal <apal60525@gmail.com>
2025-10-18 15:34:49 +0530

    "Java basic - Pattern 13th"

--- DSA_2_CP/src/Fundamental/Pattern_All.java
+++ DSA_2_CP/src/Fundamental/Pattern_All.java
@@ -18,7 +18,8 @@ public class Pattern_All {
 //        nineth(n);
 //        tenth(n);
 //        eleventh(n);
-        twelveth(n);
+//        twelveth(n);
+        thirteenth(n);
 
 
 
@@ -231,6 +232,19 @@ public class Pattern_All {
 
             }
             System.out.println(" ");
+        }
+    }
+
+    public static void thirteenth(int n){
+        for(int i =0 ; i<n; i++){
+            int icj = 1;
+            for(int j = 0 ; j<=i; j++){
+                System.out.print (icj + "\t");
+                int icjp1 = icj*(i-j)/(j+1);
+                icj = icjp1;
+            }
+            System.out.println(" ");
+
         }
     }
 }
