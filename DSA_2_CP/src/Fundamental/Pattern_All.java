8fddb0582a875639ba6f2d9dc27030c38cc82c65
Aman Pal <apal60525@gmail.com>
2025-10-20 16:15:30 +0530

    "Java basic - Pattern 16th"

--- DSA_2_CP/src/Fundamental/Pattern_All.java
+++ DSA_2_CP/src/Fundamental/Pattern_All.java
@@ -21,7 +21,8 @@ public class Pattern_All {
 //        twelveth(n);
 //        thirteenth(n);
 //        fourteenth(n);
-        fitheenth(n);
+//        fitheenth(n);
+        sixteenth(n);
 
 
 
@@ -274,8 +275,6 @@ public class Pattern_All {
                 }else {
                     countin--;
                 }
-
-
             }
             if(i<=n/2){
                 sp--;
@@ -290,6 +289,34 @@ public class Pattern_All {
             System.out.println(" ");
 
 
+        }
+
+    }
+
+    public static void sixteenth(int n) {
+        int sp = 2*n-3;
+        int st = 1;
+        for(int i = 1; i<=n; i++){
+            int val = 1;
+            for(int j = 1; j<=st; j++){
+                System.out.print (val+"\t");
+                val++;
+
+            }
+            for(int j = 1; j<=sp; j++){
+                System.out.print ("\t");
+            }
+            if(i==n){
+                st--;
+                val--;
+            }
+            for(int j= 1;j<=st; j++){
+                val--;
+                System.out.print (val+"\t");
+            }
+            st++;
+            sp -=2;
+            System.out.println ();
         }
 
     }
