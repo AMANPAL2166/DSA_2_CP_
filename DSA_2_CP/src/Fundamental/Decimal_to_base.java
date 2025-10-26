7dd356bdd6d330a7506c0cff2a618aaac546671b
Aman Pal <apal60525@gmail.com>
2025-10-26 12:40:57 +0530

    "Java basic - get sum from any base"

--- DSA_2_CP/src/Fundamental/Decimal_to_base.java
+++ DSA_2_CP/src/Fundamental/Decimal_to_base.java
@@ -2,12 +2,13 @@ package Fundamental;
 
 public class Decimal_to_base {
     public static void main(String[] args) {
-        int n = 172;
-        int b = 8;
-        int c = 2;
+        int n = 8;
+        int b = 236;
+        int c = 754;
 //        decimal_base(n,b);
 //        base_decimal(n,b);
-        System.out.println(getValue(n, b, c));
+//        System.out.println(getValue(n, b, c));
+        System.out.println(getsum(n, b , c));
     }
 
     public static  int decimal_base(int n, int b) {
@@ -40,5 +41,24 @@ public class Decimal_to_base {
         int dec = decimal_base(n,b1);
         int dn = base_decimal(dec, b2);
         return dn;
+    }
+
+    public static int getsum(int b, int b1, int b2) {
+        int rv = 0;
+        int c= 0;
+        int p = 1;
+        while(b1>0 || b2>0 || c>0){
+            int d1 = b1%10;
+            int d2 = b2%10;
+            b1 = b1/10;
+            b2 = b2/10;
+            int d = d1+d2+ c;
+            c = d/b;
+            d = d%b;
+            rv +=d*p;
+            p = p *10;
+        }
+        return rv;
+
     }
 }
