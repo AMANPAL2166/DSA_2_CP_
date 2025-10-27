d070824953a97d174a22811f536831d13f866e3d
Aman Pal <apal60525@gmail.com>
2025-10-27 17:25:52 +0530

    "Array - Find of the element in array"

--- DSA_2_CP/src/Array/Span.java
+++ DSA_2_CP/src/Array/Span.java
@@ -1,9 +1,8 @@
 package Array;
 
 public class Span {
-    public static void main(String[] arg) {
-        int[] arr = {10, 20, 30, 40, 50};
-        int max = Integer.MIN_VALUE;
+    public static void span(int[] arr) {
+         int max = Integer.MIN_VALUE;
         int min = arr[0];
         for(int i = 0; i<arr.length; i++){
             if(arr[i] > max){
@@ -17,5 +16,24 @@ public class Span {
         System.out.println("MIN digit in the element: "+ min);
         int span = max-min;
         System.out.println("Span of a number is: " + span);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        span(arr);
+        find_ele(arr, 20);
+    }
+
+    public static void find_ele(int[] arr, int d) {
+        int idx = -1;
+        for(int i =0; i< arr.length; i++){
+            if(arr[i]==d){
+                idx = i;
+                break;
+            }
+
+        }
+        System.out.println("Index of this element: "+idx);
+
     }
 }
