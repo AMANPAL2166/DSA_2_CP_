d0d20ca5131741257310b26e0192c84287f4143e
Aman Pal <apal60525@gmail.com>
2025-10-27 23:29:45 +0530

    "Array - Bar plot in  array"

--- DSA_2_CP/src/Array/Span.java
+++ DSA_2_CP/src/Array/Span.java
@@ -19,9 +19,10 @@ public class Span {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] arr = {3, 4, 0, 5, 2};
         span(arr);
         find_ele(arr, 20);
+        barplot(arr);
     }
 
     public static void find_ele(int[] arr, int d) {
@@ -35,5 +36,25 @@ public class Span {
         }
         System.out.println("Index of this element: "+idx);
 
+    }
+
+    public static void barplot(int[] s) {
+        int max = s[0];
+        for(int i = 1; i<s.length; i++){
+            if(s[i]>max){
+                  max=s[i];
+            }
+        }
+        for(int i=max; i>=1; i--){
+            for(int j =0; j<s.length;j++){
+                if(s[j]>= i){
+                    System.out.print ("*\t");
+                }else {
+                    System.out.print ("\t");
+                }
+            }
+            System.out.println();
+
+        }
     }
 }
