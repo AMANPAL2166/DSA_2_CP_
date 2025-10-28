86a0e73b7893e984d24d9c3f75c973dda008391d
Aman Pal <apal60525@gmail.com>
2025-10-28 18:58:05 +0530

    "Stack- Basics"

--- DSA_2_CP/src/Stack/Basics.java
+++ DSA_2_CP/src/Stack/Basics.java
@@ -12,19 +12,21 @@ public class Basics {
         System.out.println(st +" Peek ->" + st.peek()+" ") ;
         st.pop();
         st.pop();
-        st.push(22);
+        st.push(59);
         System.out.println(st +" Peek ->" + st.peek()+" ") ;
-//        st.pop();
-//        System.out.println(st +" Peek ->" + st.peek()+" ") ;
-        st.push(22);
+        st.pop();
+        System.out.println("Is stack empty? "+ st.isEmpty()) ;
+        st.push(33);
         System.out.println(st +" Peek ->" + st.peek()+" ") ;
         st.push(22);
         System.out.println(st +" Peek ->" + st.peek()+"|| Size of Stack ->" +st.size()) ;
+        System.out.println("Size of stack before pop: " + st.size());
 
-
-
-
-
+        // Use loop to pop the stack till last
+        while (!st.isEmpty()){
+            System.out.println(st.pop());
+        }
+        System.out.println("Size of stack after pop: " + st.size());
 
     }
 }
