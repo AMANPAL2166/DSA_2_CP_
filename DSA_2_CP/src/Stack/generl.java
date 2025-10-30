ca4bb4badf21132d893c8d879e9ec12ad431d891
Aman Pal <apal60525@gmail.com>
2025-10-30 15:58:04 +0530

    "Stack-Delete unused file"

--- DSA_2_CP/src/Stack/generl.java
+++
@@ -1,9 +0,0 @@
-package Stack; 
-public class generl {
-    public static void main(String[] args) {
-        System.out.println(
-                "I'm glad to inform u..!!"
-        );
-        System.out.println(" ");
-    }
-}
--- DSA_2_CP/src/Stack/onwmore.java
+++
@@ -1,7 +0,0 @@
-package Stack;
-
-public class onwmore {
-    public static void main(String[] args) {
-        System.out.println("  ");
-    }
-}
