c377d7aaa79c78fe65daa94ae312ef9e5251adcf
Aman Pal <apal60525@gmail.com>
2025-11-08 13:42:15 +0530

    "Stack-4 in max using stack " O(n)""

--- DSA_2_CP/src/Stack/four_in_Max.java
+++ DSA_2_CP/src/Stack/four_in_Max.java
@@ -2,10 +2,13 @@ package Stack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class four_in_Max {
     public static void main(String[] args) {
         int[] arr= {2,9,3,8,1,7,12,6,14,4,32,0,7,19,8,12};
+
+        //___________--- N^2---__________Time complexity--
         ArrayList<Integer> st = new ArrayList<>();
         int k = 4;
          for(int i=0; i<=arr.length-k; i++ ){
@@ -20,5 +23,40 @@ public class four_in_Max {
              i+=2;
          }
         System.out.println(st);
+         n(arr);
+    }
+
+    public static void n(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int k = 4;
+        int[] next_greater_element = new int[arr.length];
+        st.push(arr.length-1);
+        next_greater_element[arr.length-1] = arr.length;
+        for(int i= arr.length-2; i>=0;i--){
+            //pop-answer-push
+            while(st.size()>0 && arr[i] >= arr[st.peek()]){
+                st.pop();
+            }
+            if(st.size() == 0){
+                next_greater_element[arr.length-1] = arr.length;
+
+            }else{
+                next_greater_element[ i] = st.peek();
+            }
+            st.push(i);
+        }
+        int j = 0;
+        for(int i =0; i<=arr.length-k;i++ ){
+            //enter the loop to find the maximum of the window at i
+            if(j<i){
+                j=i;
+            }
+            while(next_greater_element[j] < i +k){
+                j =next_greater_element[j];
+
+            }
+            System.out.println(arr[j]);
+
+        }
     }
 }
