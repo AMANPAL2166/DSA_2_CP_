84d136698ef57b8d41809d9a061529ea9b8b28ea
Aman Pal <apal60525@gmail.com>
2025-11-12 20:11:53 +0530

    "Fundamental- Object and memory "

---
+++ DSA_2_CP/src/Fundamental/Object_and_memory.java
@@ -0,0 +1,36 @@
+package Fundamental;
+
+public class Object_and_memory {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "Classy";
+        p1.age = 33;
+//        p1.sayHi();
+        Person p2 = new Person();
+        p2.name = "Shreyash";
+        p2.age = 19;
+//        p2.sayHi();
+        p1.sayHi();
+        p2.sayHi();
+        swap(p1,p2);
+        p1.sayHi();
+        p2.sayHi();
+
+
+    }
+
+    public static class Person{
+        int age ;
+        String name;
+        void sayHi(){
+            System.out.println(name + ", I'm " +age+ "year old.");
+        }
+
+    }
+
+    public static void swap(Person p1,  Person p2) {
+        Person temp = p1;
+        p1 = p2;
+        p2 = temp;
+    }
+}
