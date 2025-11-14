9c6a8bb2dd87f29c9837951bb2b3e7ae11873329
Aman Pal <apal60525@gmail.com>
2025-11-14 14:09:56 +0530

    "Fundamental- Object and memory  "

--- DSA_2_CP/src/Fundamental/Object_and_memory.java
+++ DSA_2_CP/src/Fundamental/Object_and_memory.java
@@ -12,7 +12,7 @@ public class Object_and_memory {
 //        p2.sayHi();
         p1.sayHi();
         p2.sayHi();
-        swap(p1,p2);
+        swap1(p1,p2);
         p1.sayHi();
         p2.sayHi();
 
@@ -32,5 +32,18 @@ public class Object_and_memory {
         Person temp = p1;
         p1 = p2;
         p2 = temp;
+    }
+
+    public static void swap1(Person p1,  Person p2) {
+       p1 = new Person();
+        int age = p1.age;
+        p1.age = p2.age;
+        p2.age = age;
+
+        p2 = new Person();
+        String name = p1.name;;
+        p1.name = p2.name;
+        p2.name  = name;
+
     }
 }
