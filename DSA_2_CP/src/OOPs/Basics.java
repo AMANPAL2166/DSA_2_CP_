e55f387c6b483861deb5c50c827c7d01f8daf7c2
Aman Pal <apal60525@gmail.com>
2025-11-26 23:28:09 +0530

    "OPP's basics concept"

---
+++ DSA_2_CP/src/OOPs/Basics.java
@@ -0,0 +1,43 @@
+package OOPs;
+
+public class Basics {
+//    public static void main(String[] args) {
+//        Car x = new Car();
+//        x.name = "Maruti dzire";
+//        x.brand = "Maruti Suzuki";
+//        x.price = 444444400;
+//        x.speed = 40;
+//        System.out.println(x.name);
+//        System.out.println(x.brand);
+//        System.out.println(x.price);
+//        System.out.println(x.speed);
+//
+//    }
+//    public  static class Car{
+//        String name;
+//        int price;
+//        int speed;
+//        String brand ;
+//    }
+    //Now we have a question. Can we make class in the main function;
+   // -------Let's ' check-----
+//public static void main(String[] args) {
+//     class Car{
+//        String name;
+//        int speed;
+//        int price;
+//    }
+//    Car x = new Car();
+//     x.name = "Maruti";
+//     x.price = 20000;
+//     x.speed = 50;
+//    System.out.println(x.name);
+//    System.out.println(x.price);
+//    System.out.println(x.speed);
+//
+//}
+
+// Soo the answer is, yes we can but here is the catch point -- we can't use this class outside of the main function this is the main drawback;;;;;
+
+
+}
