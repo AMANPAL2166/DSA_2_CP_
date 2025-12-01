5f44bc6edf3a6e98a352a33dec19715ad8589bc9
Aman Pal <apal60525@gmail.com>
2025-12-01 18:40:59 +0530

    "Palindrome check"

---
+++ DSA_2_CP/src/String/Palindrome.java
@@ -0,0 +1,34 @@
+package String;
+public class Palindrome {
+    public static void main(String[] args) {
+        String word = "Amma";
+        System.out.println(isPalindrome(word));
+
+
+
+    }
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst )) {
+                start++;
+            } else if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+
+    }
+}
