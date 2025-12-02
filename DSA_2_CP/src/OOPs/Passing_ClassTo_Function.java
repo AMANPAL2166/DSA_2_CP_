74a8c42d110ec435a31e1d1cc86a54ba464d8397
Aman Pal <apal60525@gmail.com>
2025-12-02 23:56:45 +0530

    "OOP's"

--- DSA_2_CP/src/OOPs/Passing_ClassTo_Function.java
+++ DSA_2_CP/src/OOPs/Passing_ClassTo_Function.java
@@ -20,6 +20,31 @@ public class Passing_ClassTo_Function {
     public static void main(String[] args) {
         Student s1 = new Student("Rahul", 90);
         printStudent(s1);
+        Counter c1 = new Counter(88);
+        System.out.println(c1.value);
+    }
+    //second class;
+    static class Counter{
+        int value;
+        Counter(int value){
+            this.value = value;
+        }
+    }
+    class Main2{
+        //method that change the state of the same object;
+        static void increment(Counter c){
+            c.value++;
+
+        }
+
+        public static void main(String[] args) {
+            Counter c1 = new Counter(5);
+            System.out.println("Before:  " + c1.value);
+            increment(c1);
+            System.out.println("After: " + c1.value);
+
+
+        }
     }
 
 }
