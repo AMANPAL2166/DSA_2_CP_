7e78cc32ce2de03a556e4f7320decc4b8679fd32
Aman Pal <apal60525@gmail.com>
2025-12-05 23:42:54 +0530

    "OOP's"

--- DSA_2_CP/src/OOPs/Inheritence.java
+++ DSA_2_CP/src/OOPs/Inheritence.java
@@ -2,6 +2,6 @@ package OOPs;
 
 public class Inheritence {
     public static void main(String[] args) {
-        System.out.println("Inheritance.....");
+        System.out.println("Inheritance.....!!");
     }
 }
