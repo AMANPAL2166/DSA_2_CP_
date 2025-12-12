8740366ad7c506a1354c477f3ae81a27a64a3133
Aman Pal <apal60525@gmail.com>
2025-12-12 21:59:24 +0530

    "String- Pair sum in sorted array{Naive Approach }"

--- DSA_2_CP/src/Array/Pair_Sum_In_sortedArray.java
+++ DSA_2_CP/src/Array/Pair_Sum_In_sortedArray.java
@@ -1,12 +1,13 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Pair_Sum_In_sortedArray {
     public static void main(String[] args) {
         int[] arr = {3,1,3,7,8,0};
-        int target = 10;
-        System.out.println(pairInSortedRotated(arr, target));
+        int target = 2;
+        System.out.println(pairInSortedRotated_1(arr, target));
 
     }
 
@@ -35,6 +36,19 @@ public class Pair_Sum_In_sortedArray {
         return  false;
 
 
+    }
 
+    public static boolean pairInSortedRotated_1(int[] arr, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0; i<arr.length; i++){
+            //find difference between arr[i] to target
+            int difference = target -arr[i];
+            //Check if the difference exist in the set
+            if(set.contains(difference)){
+                return true;
+            }
+            set.add(arr[i]);
+        }
+        return false;
     }
 }
