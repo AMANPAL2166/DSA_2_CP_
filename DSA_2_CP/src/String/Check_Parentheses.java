f43e480a3e6f6b5957820b050e6eb887e5d60254
Aman Pal <apal60525@gmail.com>
2025-12-12 19:13:03 +0530

    "String- Check Parentheses{It's valid check}"

--- DSA_2_CP/src/String/Check_Parentheses.java
+++ DSA_2_CP/src/String/Check_Parentheses.java
@@ -1,9 +1,11 @@
 package String;
 
+import java.util.Stack;
+
 public class Check_Parentheses {
     public static void main(String[] args) {
         String r = "([)]";
-        System.out.println(isValid(r));
+        System.out.println(isValid_1(r));
 
     }
     //In this method we can't handle nesting and order, which are the core of the Valid Parentheses problem
@@ -22,6 +24,55 @@ public class Check_Parentheses {
             }
         }
         return false;
+
+    }
+
+    public static boolean isValid_1(String s) {
+        // Create a new stack to store the characters
+        Stack<Character> stack = new Stack<>();
+        //convert string into char array and access the character using for loop.
+        for(char ch: s.toCharArray())
+        {
+            // check ch
+            switch (ch)
+            {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                    // hence it is invalid.
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+
+        // After the loop we have to check one more condition.
+        // return true only if the stack is empty.
+        // if stack is not empty that means we have unused brackets.
+
+        return stack.isEmpty();
 
     }
 }
