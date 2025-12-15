47bdd1907d3aebcc8e68c56d6f8223c3f89fea06
Aman Pal <apal60525@gmail.com>
2025-12-15 16:56:20 +0530

    "Sorting- Permutation in Array "

---
+++ DSA_2_CP/src/Sorting/PermutationInArray.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/// / Question::-- Given 2 array, condition is arr1[i]+ arr2[i]>=k then we return true otherwise  return false.
+/// Approach:::- We can sort the array in a way like one with ascending and another one with descending. Soo that we can meet greatest arr of i with smallesst one.
+
+public class PermutationInArray {
+    public static void main(String[] args) {
+        int[] arr1  = {1, 2, 2, 1};
+        int[] arr2 = {3, 3, 3, 4};
+        int targe  = 5;
+        System.out.println(isPossible(targe, arr1, arr2));
+
+
+    }
+    public static boolean isPossible(int k, int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for(int i = 0; i<arr2.length/2; i++){
+            int temp = arr2[i];
+            arr2[i] = arr2[arr2.length-1-i];
+            arr2[arr2.length-1-i] = temp;
+        }
+        //check if every pair sum  is greater than k
+        for(int i = 0; i<arr2.length; i++){
+            if(arr1[i] + arr2[i] <k){
+                return false;
+            }
+        }
+        return true;
+    }
+}
