9583ee7ac09cc3fd9942323aae9b6c167823d929
Aman Pal <apal60525@gmail.com>
2025-12-17 23:54:09 +0530

    "String:- Solved{ Convert a sentence into numeric" }

---
+++ DSA_2_CP/src/String/Convert_A_sentenceInto_Numeric.java
@@ -0,0 +1,36 @@
+package String;
+
+public class Convert_A_sentenceInto_Numeric {
+    public static void main(String[] args) {
+        String str[]
+                = { "2",    "22",  "222", "3",   "33", "333",
+                "4",    "44",  "444", "5",   "55", "555",
+                "6",    "66",  "666", "7",   "77", "777",
+                "7777", "8",   "88",  "888", "9",  "99",
+                "999",  "9999" };
+
+        String input = "GEEKSFORGEEKS";
+        System.out.println(printSequence(str, input));
+    }
+
+    public static String printSequence(String  arr[], String input) {
+        String output = "";
+        //length of input string
+        int n = input.length();
+        for(int i = 0; i<n; i++){
+            //checking for space
+            if(input.charAt(i) == ' '){
+                output = output+"0";
+            }else{
+                //calculating index for each character
+                int position = input.charAt(i) -'A';
+                output = output + arr[position];
+
+            }
+        }
+        //output sequence
+        return output;
+
+
+    }
+}
