7a51bee2f0151e81b2173fcb1d7c1671d7d18daf
Aman Pal <apal60525@gmail.com>
2026-01-15 16:17:45 +0530

    "Linkedlist:- Insertion in Linkedlist {Insertion in anywhere}

--- DSA_2_CP/src/LinkedList/Operations/Insertion.java
+++ DSA_2_CP/src/LinkedList/Operations/Insertion.java
@@ -15,6 +15,33 @@ public class Insertion {
         list.next = head;
         return list;
     }
+    //Function to insert a new node after given node
+    public static Node insertAfter(Node head, int key, int new_data){
+        //initialize a new pointer to current head
+        Node curr = head;
+        //Iterate over a linked list to find the key
+        while(curr!=null){
+            if(curr.data == key){
+                break;
+            }
+            //update next
+            curr = curr.next;
+        }
+        //If current become null, it means given key is not found in linkedlist
+        if(curr == null){
+            System.out.println("Node is not found");
+            return head;
+        }
+        //Allocate the new Node and make the element to be inserted  as a new Node
+        Node newNode = new Node(new_data);
+        //set the next pointer of new node to the next pointer of given node
+        newNode.next = curr.next;
+        //Point the current node next to the new node
+        curr.next = newNode;
+        //return the head to the modified linkedlist
+        return head;
+
+    }
     //Print function
     public static void PrintLinkedlist(Node head){
         Node curr = head;
@@ -29,13 +56,22 @@ public class Insertion {
         System.out.println();
     }
 
+
     public static void main(String[] args) {
         Node list = new Node(2);
         list.next = new Node(33);
         list.next.next = new Node(22);
         int key = 44;
+        System.out.print("Linkedlist Before insertion: ");
         PrintLinkedlist(list);
         list = insertAtfront(list, key);
+        System.out.print("Linkedlist After insertion: ");
+        PrintLinkedlist(list);
+        System.out.println("Insert new node in current Node");
+        int new_key = 2;
+        int New_data = 999;
+        list = insertAfter(list,new_key,New_data );
+        System.out.print("Insert at 2 node: " );
         PrintLinkedlist(list);
 
     }
