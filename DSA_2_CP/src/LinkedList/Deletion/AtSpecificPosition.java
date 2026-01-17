32e90f290637173c37e0ef8f6ade46cecdee4c1d
Aman Pal <apal60525@gmail.com>
2026-01-17 18:47:32 +0530

    "Linkedlist:- Deletion in Linkedlist {at specific position  }

---
+++ DSA_2_CP/src/LinkedList/Deletion/AtSpecificPosition.java
@@ -0,0 +1,56 @@
+package LinkedList.Deletion;
+
+public class AtSpecificPosition {
+    static class Node{
+        int data;
+        Node next;
+        Node(int new_data){
+            data = new_data;
+            next = null;
+        }
+    }
+    //Print
+    public static void PrintLinkedlist(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print (curr.data);
+            if(curr.next != null){
+                System.out.print ("<->");
+            }
+            //update curr
+            curr = curr.next;
+        }
+    }
+    public static Node DeleteAtSpecific(Node head, int key){
+        //Create a temp variable
+        Node temp = head;
+        //Check if key is one then remove head
+        if(key == 1){
+            head = temp.next;
+            return head;
+        }
+        //Traverse to the node before the one to be deleted
+        //create a prev variable
+        Node prev = null;
+        for(int i = 1;i<key; i++){
+            prev= temp;
+            temp = temp.next;
+        }
+        //Delete the node at the position
+        prev.next = temp.next;
+        return head;
+
+    }
+    public static void main(String[] args) {
+         Node newlist = new Node(1);
+         newlist.next = new Node(2);
+         newlist.next.next = new Node(3);
+         newlist.next.next.next = new Node(4);
+
+         int key  = 4;
+         newlist = DeleteAtSpecific(newlist, key);
+         PrintLinkedlist(newlist);
+
+    }
+
+}
