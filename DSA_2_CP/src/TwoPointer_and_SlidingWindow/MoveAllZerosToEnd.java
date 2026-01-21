fead85dcb18786c601da260257e0fb6f2ea8a0ab
Aman Pal <apal60525@gmail.com>
2026-01-21 23:55:13 +0530

    "Two pointer:- Remove all occurrence.

--- DSA_2_CP/src/TwoPointer_and_SlidingWindow/MoveAllZerosToEnd.java
+++ DSA_2_CP/src/TwoPointer_and_SlidingWindow/MoveAllZerosToEnd.java
@@ -42,9 +42,34 @@ public class MoveAllZerosToEnd {
         return arr;
     }
 
+
     public static void main(String[] args) {
         int[]arr = {1,3,2,0,2};
         System.out.println(Arrays.toString(pushzero(arr)));
         System.out.println(Arrays.toString(pushzero2(arr)));
     }
+    static void pushZerosToEnd(int[] arr) {
+
+        // Pointer to track the position
+        // for next non-zero element
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // If the current element is non-zero
+            if (arr[i] != 0) {
+
+                // Swap the current element with
+                // the 0 at index 'count'
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                // Move 'count' pointer to
+                // the next position
+                count++;
+            }
+        }
+    }
+
 }
