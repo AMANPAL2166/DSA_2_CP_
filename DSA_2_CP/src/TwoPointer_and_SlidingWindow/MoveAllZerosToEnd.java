80c9f20ecf3912ae1bf9e602ef1d801b163427a3
Aman Pal <apal60525@gmail.com>
2026-01-21 23:22:30 +0530

    "Two pointer:- Remove all occurrence.

---
+++ DSA_2_CP/src/TwoPointer_and_SlidingWindow/MoveAllZerosToEnd.java
@@ -0,0 +1,50 @@
+package TwoPointer_and_SlidingWindow;
+
+import java.util.Arrays;
+
+public class MoveAllZerosToEnd {
+//    Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+//    Output: [1, 2, 4, 3, 5, 0, 0, 0]
+///    Explanation: There are three 0s that are moved to the end.
+    //Solution:: How i can move all the zeros to the end.
+    //1) Naive approach - use temporary array
+    static int[] pushzero(int[] arr){
+        //declare new array of the size of given array
+        //we don't need extra zeros just have a extra variable
+        int count = 0;
+        for(int i = 0; i<arr.length ; i++){
+            if(arr[i]!=0){
+                 arr[count] = arr[i];
+                 count++;
+            }
+
+        }
+//        fill the rest of the array with 0
+        while(count<arr.length){
+            arr[count] = 0;
+            count++;
+        }
+        return arr;
+    }
+    //Two traversal function
+    static int[] pushzero2(int[] arr){
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] != 0 ){
+                arr[count++] = arr[i];
+
+            }
+        }
+        while(count<arr.length){
+            arr[count++] = 0;
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[]arr = {1,3,2,0,2};
+        System.out.println(Arrays.toString(pushzero(arr)));
+        System.out.println(Arrays.toString(pushzero2(arr)));
+    }
+}
