11b48336947ae4d90fb557f62ff4a7516e62eb7d
Aman Pal <apal60525@gmail.com>
2026-01-23 22:56:50 +0530

    "Two Pointer:- Two sum using two pointer .

---
+++ DSA_2_CP/src/TwoPointer_and_SlidingWindow/TwoSum.java
@@ -0,0 +1,31 @@
+package TwoPointer_and_SlidingWindow;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = { 0, -1, 2, -3, 1} ;
+        int target = -1;
+        System.out.println(twoSum(arr,target));
+
+    }
+    static boolean twoSum(int arr[], int target) {
+        // code here
+        Arrays.sort(arr);
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        while(start<end){
+            int sum = arr[start]+arr[end];
+            if(sum == target){
+                return true;
+            }else if(sum>target){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return false;
+
+    }
+}
