2fa373fceec94c4274b477f9fbf175bd3582b8cc
Aman Pal <apal60525@gmail.com>
2026-01-29 16:18:10 +0530

    "LinkedList:-Creating my own linklist  .

--- DSA_2_CP/src/LinkedList/Linked_List.java
+++ DSA_2_CP/src/LinkedList/Linked_List.java
@@ -13,7 +13,7 @@ public class Linked_List {
     public static Node head;
     public static Node tail;
 //------------Print function--------------
-    public static void print(Node head){
+    public void print(Node head){
         Node curr = head;
         while(curr != null){
             System.out.print (curr.data);
@@ -27,29 +27,43 @@ public class Linked_List {
 //    public static Node head;
 //    public static Node tail;
 //------------Insert data at head---------
-    static Node insertFirst(int data ){
+    public void   addFirst(int data ){
         //Step 1st - Create new node
         Node newlist = new Node(data);
         //Base case, if our list is empty
         if(head == null){
             head = tail  = newlist;
-            return newlist;
+
         }
 
         //Step 2 - Assign newlist next = head;
         newlist.next = head;//Linking step
         //Step 3 - newlist = head;
         newlist = head;
-        return head;
 
+
+    }
+    public void addLast(int data){
+        //Step 1 : Create a new Node
+        Node newNode = new Node(data);
+        /// Check base cases
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        //Step 2: link newNode.next = tail;
+        newNode.next  =tail;
+        //Step 3: assign tail = newNode
+        tail = newNode;
     }
 
     public static void main(String[] args) {
         Linked_List ll = new Linked_List();
-        Node newList = new Node(88);
-        newList.next = new Node(33);
-        newList = insertFirst(22);
-        print(newList);
+        ll.head = new Node(1);
+        head.next = new Node(2);
+        ll.addFirst(0);
+        ll.print( );
 
     }
 
