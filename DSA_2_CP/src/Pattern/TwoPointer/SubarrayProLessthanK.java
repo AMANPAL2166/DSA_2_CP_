6cdc2fa1d85e231cb33000499797936f2d590130
Aman Pal <apal60525@gmail.com>
2026-02-05 23:57:44 +0530

    "Patterns:-  Subarray of product.

---
+++ DSA_2_CP/src/Pattern/TwoPointer/SubarrayProLessthanK.java
@@ -0,0 +1,26 @@
+package Pattern.TwoPointer;
+
+public class SubarrayProLessthanK {
+    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+        //Brute force
+        int count = 1;
+
+        for(int i = 0; i<nums.length;i++){
+            int pro = 1;
+            for(int j = i; j<nums.length;j++){
+                pro *= nums[j];
+                if(pro <k){
+                    count++;
+                }else{
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr= {10,5,2,6};
+        System.out.println(numSubarrayProductLessThanK(arr,100));
+    }
+}
