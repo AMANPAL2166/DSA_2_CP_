db3b96a5bfe0c73c3278ee98d62b7717f0cbe404
Aman Pal <apal60525@gmail.com>
2026-02-05 23:07:41 +0530

    "Patterns:-  Square of sorted array.

--- DSA_2_CP/src/Pattern/TwoPointer/SqureOfSorted.java
+++ DSA_2_CP/src/Pattern/TwoPointer/SqureOfSorted.java
@@ -30,11 +30,34 @@ public class SqureOfSorted {
 
 
     }
+    public static int[] sortedSquares2(int[] nums){
+        //Optimal method for square of array without using sort
+        int[] ans = new int[nums.length];
+        int start = 0;
+        int end= nums.length-1;
+        int pos = nums.length-1;
+        while(start<=end){
+            //square of left element
+            int leftSqr = nums[start]*nums[start];
+            //square of right element
+            int rightSqr=nums[end]*nums[end];
+            //check if rightsqr > leftsqr:then pos == rightsqr. Because larger element in the last
+            if(leftSqr<rightSqr){
+                ans[pos--] = rightSqr;
+                end--;
+            }else{
+                ans[pos--] = leftSqr;
+                start++;
+            }
+
+        }
+        return ans;
+    }
 
 
     public static void main(String[] args) {
         int[] arr = {-7,-3,2,3,11};
-        System.out.println(Arrays.toString(sortedSquares1(arr)));
+        System.out.println(Arrays.toString(sortedSquares2(arr)));
     }
 
 }
