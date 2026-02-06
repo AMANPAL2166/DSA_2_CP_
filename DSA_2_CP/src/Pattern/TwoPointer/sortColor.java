863a1066645da1ae8cc24027f5af1a3fee1fde8a
Aman Pal <apal60525@gmail.com>
2026-02-07 00:09:59 +0530

    "Pattern:-  sort color.

---
+++ DSA_2_CP/src/Pattern/TwoPointer/sortColor.java
@@ -0,0 +1,43 @@
+package Pattern.TwoPointer;
+
+import java.util.Arrays;
+
+public class sortColor {
+    public static void sortColors(int[] nums) {
+        int[] ans = new int[nums.length];
+        int idx  = 0;
+        for(int i = 0; i<nums.length;i++){
+            if(nums[i] == 0) {
+                ans[idx] = nums[i];
+                idx++;
+            }
+
+        }
+        idx = idx;
+        for(int i = 0; i<nums.length;i++){
+            if(nums[i] == 1){
+                ans[idx] = nums[i];
+                idx++;
+            }
+        }
+        idx = idx;
+
+        for(int i = 0; i<nums.length ;i++){
+            if(nums[i] == 2){
+                ans[idx] = nums[i];
+                idx++;
+            }
+        }
+        for(int i = 0; i<nums.length;i++){
+            nums[i]= ans[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,0,2,1,1,0};
+        sortColors(arr);
+    }
+}
