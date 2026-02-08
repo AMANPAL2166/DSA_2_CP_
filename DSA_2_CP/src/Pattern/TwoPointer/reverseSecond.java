79a180dcf93e1567ad8b448925c6ddeabb723b64
Aman Pal <apal60525@gmail.com>
2026-02-08 23:31:50 +0530

    "Pattern:- Reverse .

---
+++ DSA_2_CP/src/Pattern/TwoPointer/reverseSecond.java
@@ -0,0 +1,11 @@
+package Pattern.TwoPointer;
+
+public class reverseSecond {
+    public void   reverseStr(String s, int k) {
+        int start = 0;
+        while(start<k){
+            int temp = s.charAt(k);
+
+        }
+    }
+}
