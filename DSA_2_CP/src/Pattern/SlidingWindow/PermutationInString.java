176e59be498046e98d0e953a682ec80e62c9b051
Aman Pal <apal60525@gmail.com>
2026-02-10 23:15:34 +0530

    "Pattern:- Permutation in  string.

--- DSA_2_CP/src/Pattern/SlidingWindow/PermutationInString.java
+++ DSA_2_CP/src/Pattern/SlidingWindow/PermutationInString.java
@@ -1,17 +1,25 @@
 package Pattern.SlidingWindow;
 
+import java.util.Arrays;
+
 public class PermutationInString {
     public static boolean checkInclusion(String s1, String s2) {
-        char[] str1 = s1.toCharArray();
-        char[] str2 = s2.toCharArray();
-        for(int i = 0; i<str2.length; i++){
-             int j = i+1;
-             while(j<str1.length){
-
-             }
+         //Get length of s1
+        int k = s1.length();
+        for(int i = 0;i<=s2.length()-k; i++){
+            //create new string to check that specific window to the length of k
+            String sub = s2.substring(i, i+k);
+            //create two char variable
+            char[] a = s1.toCharArray();
+            char[] b= sub.toCharArray();
+            //Now sort
+            Arrays.sort(a);
+            Arrays.sort(b);
+            if(Arrays.equals(a,b)){
+                return true;
+            }
         }
         return false;
-
     }
 
     public static void main(String[] args) {
