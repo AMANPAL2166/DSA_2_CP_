ec9d40fd7563d43d4c2134cefb96791aafe26b0b
Aman Pal <apal60525@gmail.com>
2026-02-14 23:06:55 +0530

    "Pattern :-Pivot sum of left and right index .

--- DSA_2_CP/src/Pattern/Prefix/PivotIndex.java
+++ DSA_2_CP/src/Pattern/Prefix/PivotIndex.java
@@ -1,29 +1,51 @@
 package Pattern.Prefix;
 
+import java.util.Arrays;
+
 public class PivotIndex {
     public  static int pivotIndex(int[] nums) {
-        int i = 1;
-        int j = nums.length-1;
-        int leftSum = nums[0], rightSum = nums[j];
-        
-        while(i<=j-1){
-
-            if(leftSum==rightSum){
-                return i;
-            } else if (leftSum<rightSum) {
-                leftSum += nums[i];
-                i++;
-            }else{
-                rightSum += nums[j-1];
-                j--;
+        for(int i = 0;i<nums.length;i++){
+            int leftSum = 0;
+            int rightSum =0;
+            //this loop run for leftSum
+            for(int j= 0; j<i;j++){
+                leftSum+= nums[j];
+            }//this loop for rightSum
+            for(int j = i+1; j<nums.length;j++){
+                rightSum+= nums[j];
             }
+            //This is checking
+            if(leftSum == rightSum){
+                return i;
+            }
+
         }
         return -1;
+
+    }
+    public  static int pivotIndexOPT(int[] nums) {
+         //Store all the prefix;
+
+         int total = 0;
+         for(int num: nums){
+             total+=num ;
+         }
+         int leftSum  = 0;
+         for(int i = 0; i<nums.length;i++){
+             int rightSum = total -leftSum-nums[i];
+             if(rightSum==leftSum){
+                 return i;
+             }
+             leftSum+= nums[i];
+         }
+         return -1;
+
 
     }
 
     public static void main(String[] args) {
         int[] arr = {1,7,3,6,5,6};
-        System.out.println(pivotIndex(arr));
+        System.out.println("Brute  "+pivotIndex(arr));
+        System.out.println("Opt::"+pivotIndexOPT(arr));
     }
 }
