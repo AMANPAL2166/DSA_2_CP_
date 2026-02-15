3395f9c9372445c9d795c18b860a1406c1fe8cfa
Aman Pal <apal60525@gmail.com>
2026-02-15 23:51:25 +0530

    "Pattern:-Minimum size subarray.

---
+++ DSA_2_CP/src/Pattern/Prefix/minSizeSubarray.java
@@ -0,0 +1,31 @@
+package Pattern.Prefix;
+
+public class minSizeSubarray {
+    public static int minSubArrayLen(int target, int[] nums) {
+         for(int i = 0;i< nums.length;i++){
+             if(nums[i] == target){
+                 return 1;
+             }
+         }
+         int count = Integer.MAX_VALUE;
+         for(int i = 0;i< nums.length;i++ ){
+             int sum = 0;
+             int localCount = 1;
+             for(int j= i ;j<nums.length; j++){
+                 sum+= nums[j];
+                 if(sum>= target){
+                     count = Math.min(count, localCount);
+                 }else{
+                     localCount++;
+                 }
+             }
+
+         }
+         return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1,1,1,1};
+        System.out.println(minSubArrayLen(11, arr));
+    }
+}
