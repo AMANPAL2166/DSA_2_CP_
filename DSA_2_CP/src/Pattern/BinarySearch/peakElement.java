37879c1af2aa8a938fbf8c4ecfb871ec0acd828c
Aman Pal <apal60525@gmail.com>
2026-02-21 22:26:36 +0530

    "Binary Search:-Peak element in array.

--- DSA_2_CP/src/Pattern/BinarySearch/peakElement.java
+++ DSA_2_CP/src/Pattern/BinarySearch/peakElement.java
@@ -1,7 +1,25 @@
 package Pattern.BinarySearch;
 
 public class peakElement {
-//    public int findPeakElement(int[] nums) {
-//
-//    }
+    public static int findPeakElement(int[] nums) {
+        int start = 0;//track index
+        int end = nums.length-1;
+        //run the loop till the end -1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]<nums[mid+1]){
+                start= start+1;
+            }else{
+                end = mid;
+            }
+        }
+        return start;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(findPeakElement(nums));
+
+    }
 }
