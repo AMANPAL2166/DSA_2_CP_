437ef09c8e7345d2e53b054d4ad2aa5398b93b73
Aman Pal <apal60525@gmail.com>
2026-02-27 13:05:19 +0530

    "Binary search:-checking file..

---
+++ DSA_2_CP/src/Patterns/BinarySearch/NameFile.java
@@ -0,0 +1,16 @@
+package Patterns.BinarySearch;
+
+public class NameFile {
+    public static void main(String[] args) {
+        int[] nums= {1,2,3,4,5};
+        System.out.println(returnfirstIndex(nums));
+
+
+    }
+
+    public static int returnfirstIndex(int[] nums) {
+        return nums[0];
+    }
+
+
+}
