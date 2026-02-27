6b60a35b462b7d6aea13e60612c9bf46bc4f8a72
Aman Pal <apal60525@gmail.com>
2026-02-27 22:48:50 +0530

    "Binary search:-Given Nearly sorted array, we need to find target.

---
+++ DSA_2_CP/src/Pattern/BinarySearch/NearlySorted.java
@@ -0,0 +1,32 @@
+package Pattern.BinarySearch;
+
+public class NearlySorted {
+    public static int NearSorte(int[] arr, int target) {
+        int  start  = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid] == target){
+                return mid;
+            } else if (mid-1>=start && arr[mid+1] == target) {
+                return mid+1;
+            } else if (mid+1 <= end && arr[mid-1] == target) {
+                return mid-1;
+            }
+            if(arr[mid] > target){
+                end = mid-2;
+            }else{
+                start = mid+2;
+            }
+        }
+        return -1;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10 ,3 ,40 ,20, 50 ,80, 70};
+        System.out.println(NearSorte(nums,90));
+    }
+}
