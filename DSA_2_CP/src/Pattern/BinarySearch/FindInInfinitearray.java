5d0b1c783688e42b6d4edf8e5af5c71da306ca70
Aman Pal <apal60525@gmail.com>
2026-03-01 23:31:42 +0530

    "Binary Search:-Find elelment in infinite array.

--- DSA_2_CP/src/Pattern/BinarySearch/FindInInfinitearray.java
+++ DSA_2_CP/src/Pattern/BinarySearch/FindInInfinitearray.java
@@ -4,12 +4,18 @@ public class FindInInfinitearray {
     public static int findInINfin(int[] nums, int target){
         //we don't know last index so that first we need to find end which is greater than target
         int st  = 0, end = 1;
-        while(target>nums[end]){
+        while(nums[end]<target && end<nums.length){
             st = end;
             end = end*2;
+            //edge case
+            if(end>=nums.length){
+                end= nums.length-1;
+            }
+
+
         }
         //new we can use binary search
-        while(st<  end-1 ){
+        while(st<= end ){
             int mid = st+(end-st)/2;
             if(nums[mid] == target){
                 return mid;
