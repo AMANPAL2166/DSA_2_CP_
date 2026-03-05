1ac59b440c296f836e5b8a2d83fb2bb48e0ec837
Aman Pal <apal60525@gmail.com>
2026-03-05 23:55:13 +0530

    "Daily Question:-Min change in a string to become alternate character.

--- DSA_2_CP/src/Daily_Question/MinChange.java
+++ DSA_2_CP/src/Daily_Question/MinChange.java
@@ -2,6 +2,19 @@ package Daily_Question;
 
 public class MinChange {
     public static int minOperations(String s) {
+        int count = 0;
+        for(int i = 1;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                count++;
+            }
+            i++;
+        }
+        return count;
 
+    }
+
+    public static void main(String[] args) {
+        String s = "10010100";
+        System.out.println(minOperations(s));
     }
 }
