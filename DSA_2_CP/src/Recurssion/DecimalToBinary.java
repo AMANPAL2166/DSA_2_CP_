be2d9caddb40de0921a6fc34652949c06e68fda0
Aman Pal <apal60525@gmail.com>
2026-03-08 23:27:01 +0530

    "Recursion:-decimal to binary.

--- DSA_2_CP/src/Recurssion/DecimalToBinary.java
+++ DSA_2_CP/src/Recurssion/DecimalToBinary.java
@@ -2,24 +2,25 @@ package Recurssion;
 
 public class DecimalToBinary {
     public static void main(String[] args) {
-        int decimal = 25;
-        String binaryNumber  = "";
-        if(decimal == 0){
-            binaryNumber = "0";
-        }else{
-            int temp = decimal;
-            while(temp>0){
-                int rem = temp%2; //Get the remainder(last Bit)
-                binaryNumber = rem+binaryNumber;//Prepand the remainder to the string
-                temp = temp/2;//Divide the number by 2
-
-
-            }
-        }
-        System.out.println("Decimal- "+ decimal);
-        System.out.println("Binary- "+ binaryNumber);
-        System.out.println("Recursion solution: "+decToBin(decimal));
-        System.out.println("Recursion solution_2 : "+decToBin2(decimal));
+//        int decimal = 25;
+//        String binaryNumber  = "";
+//        if(decimal == 0){
+//            binaryNumber = "0";
+//        }else{
+//            int temp = decimal;
+//            while(temp>0){
+//                int rem = temp%2; //Get the remainder(last Bit)
+//                binaryNumber = rem+binaryNumber;//Prepand the remainder to the string
+//                temp = temp/2;//Divide the number by 2
+//
+//
+//            }
+//        }
+//        System.out.println("Decimal- "+ decimal);
+//        System.out.println("Binary- "+ binaryNumber);
+//        System.out.println("Recursion solution: "+decToBin(decimal));
+//        System.out.println("Recursion solution_2 : "+decToBin2(decimal));
+        System.out.println("Sum of digits: "+ sumOfDigit(123456));
 
     }
     public static String decToBin(int d){
@@ -36,4 +37,12 @@ public class DecimalToBinary {
         }
 
     }
+    public static int sumOfDigit(int d){
+        if(d==0){
+            return 0;
+        }
+        return (d%10 + sumOfDigit(d/10));
+
+    }
+
 }
