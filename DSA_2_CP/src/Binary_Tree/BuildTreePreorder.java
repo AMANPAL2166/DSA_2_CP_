c5d533f1ecd03cb7a234fafba9ecc58a4a67769e
Aman Pal <apal60525@gmail.com>
2026-03-08 23:28:33 +0530

    "Tree:-Getting intro of tree.

---
+++ DSA_2_CP/src/Binary_Tree/BuildTreePreorder.java
@@ -0,0 +1,39 @@
+package Binary_Tree;
+
+public class BuildTreePreorder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        //constructor
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right= null;
+        }
+    }
+    public static class BinaryTree{
+        static  int idx = -1;
+
+        public static Node buildTree(int  node[]) {
+            idx++;
+            //check
+            if(node[idx] == -1){
+                return null;
+            }
+            //create a new node
+            Node newNode = new Node(node[idx]);
+            newNode.left = buildTree(node);
+            newNode.right = buildTree(node);
+            return newNode;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(node);
+        System.out.println(root.data);
+    }
+}
