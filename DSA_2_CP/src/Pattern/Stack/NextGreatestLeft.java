213be43bfb216e4e3063f1394cf203e070b2e3dd
Aman Pal <apal60525@gmail.com>
2026-03-10 22:47:52 +0530

    "Stack:-Next Greatest to left.

--- DSA_2_CP/src/Pattern/Stack/NextGreatestLeft.java
+++ DSA_2_CP/src/Pattern/Stack/NextGreatestLeft.java
@@ -2,6 +2,9 @@ package Pattern.Stack;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Stack;
 
 public class NextGreatestLeft {
     public static ArrayList<Integer> nextGrePrev(int[] nums){
@@ -24,9 +27,30 @@ public class NextGreatestLeft {
 
         return ans;
     }
+    public static ArrayList<Integer> nextGrePrev_2(int[] nums){
+         //Storing answer and initialize with -1
+        ArrayList<Integer> ans = new ArrayList<>(Collections.nCopies(nums.length,-1));
+        //create a stack to store important value
+        Stack<Integer> st= new Stack<>();
+        //iterate till the array.length
+        for(int i =0;i<nums.length;i++ ){
+            //check stack isempty or not, if not then pop till the greater element
+            while (!st.isEmpty() && nums[i]>st.peek()){
+                st.pop();
+            }
+            //check if st.peek is greater than nums[i] then store as ans
+            if(!st.isEmpty()){
+                ans.set(i, st.peek());
+            }
+            //ans push the current value to the stack
+            st.push(nums[i]);
+
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         int[] arr = {1,11,9,5,7};
-        System.out.println(nextGrePrev(arr));
+        System.out.println(nextGrePrev_2(arr));
     }
 }
