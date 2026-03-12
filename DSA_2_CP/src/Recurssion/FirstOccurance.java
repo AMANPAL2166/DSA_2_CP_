c78398bad588e51d797dade788075572dec5906d
Aman Pal <apal60525@gmail.com>
2026-03-12 23:54:22 +0530

    "Recursion :-First Occurance.

---
+++ DSA_2_CP/src/Recurssion/FirstOccurance.java
@@ -0,0 +1,35 @@
+package Recurssion;
+
+public class FirstOccurance {
+    public static int firstOccurance(int[] arr, int tar, int idx) {
+        /// This method is good but not best, becoz we can get of target before second iteration. Write 2nd method below.
+        if(idx == arr.length){
+            return -1;
+        }
+        int firstOcc = firstOccurance(arr,tar,idx+1);
+        if(arr[idx] == tar){
+            return idx;
+
+
+        }else{
+           return firstOcc;
+        }
+    }
+    public static int firstOccurance_2(int[] arr, int tar, int idx) {
+         if(idx == arr.length) {
+             return -1;
+         }
+         if(arr[idx] == tar){
+             return idx;
+         }else{
+             int first = firstOccurance(arr, tar, idx+1);
+             return first;
+         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,22,2,2,2};
+        System.out.println("Good Approach: "+firstOccurance(arr, 2, 0));
+        System.out.println("Best Approach: " + firstOccurance_2(arr, 2, 0));
+    }
+}
