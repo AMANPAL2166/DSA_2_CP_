9e0468a61274ffc92f5932d6d92e8bb90167988a
Aman Pal <apal60525@gmail.com>
2026-04-08 23:42:22 +0530

    "LEETCODE{LINKEDLIST}:Reverse a linkedlist.

--- DSA_2_CP/src/NEETCODE/LINKED_LIST/Reverse.java
+++ DSA_2_CP/src/NEETCODE/LINKED_LIST/Reverse.java
@@ -1,14 +1,14 @@
 package NEETCODE.LINKED_LIST;
 
 public class Reverse {
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
      ListNode() {}
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
    }
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
         ListNode temp = head;
         ListNode prev = null;
         ListNode front = null;
@@ -24,4 +24,26 @@ public class Reverse {
         return prev;
 
     }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null)
+                System.out.print(" -> ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args){
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = reverseList(head);
+        printList(head);
+    }
+
 }
