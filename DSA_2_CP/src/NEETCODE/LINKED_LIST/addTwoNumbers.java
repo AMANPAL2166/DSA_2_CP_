1085056e87231191573e72c2accf910aca1b66fb
Aman Pal <apal60525@gmail.com>
2026-04-11 23:43:06 +0530

    "LEETCODE{LINKEDLIST}:Add two number.

---
+++ DSA_2_CP/src/NEETCODE/LINKED_LIST/addTwoNumbers.java
@@ -0,0 +1,38 @@
+package NEETCODE.LINKED_LIST;
+
+public class addTwoNumbers {
+    static class ListNode{
+        int data;
+        ListNode next;
+        ListNode(int new_data){
+            data = new_data;
+            next = null;
+        }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.data : 0;
+            int digit2 = (l2 != null) ? l2.data : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode newNode = new ListNode(digit);
+            tail.next = newNode;
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        ListNode result = dummyHead.next;
+        dummyHead.next = null;
+        return result;
+
+    }
+}
