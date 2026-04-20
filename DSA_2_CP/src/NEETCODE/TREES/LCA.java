2dc6947eba287290f574d2393e4e85fa59da576f
Aman Pal <apal60525@gmail.com>
2026-04-20 23:41:04 +0530

    "NEETCODE{TREE}: Find Least common ancester.

---
+++ DSA_2_CP/src/NEETCODE/TREES/LCA.java
@@ -0,0 +1,42 @@
+package NEETCODE.TREES;
+
+public class LCA {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left,TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(p.val < root.val && q.val <root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if(p.val > root.val  && q.val > root.val){
+            return lowestCommonAncestor(root.right, p,q);
+        }
+        return root;//LCA
+
+    }
+
+    public TreeNode lowestCommonAncester_(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null){
+            if(p.val < root.val && q.val < root.val){
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+
+            }else{
+                return root;
+            }
+        }
+        return null;
+
+    }
+}
