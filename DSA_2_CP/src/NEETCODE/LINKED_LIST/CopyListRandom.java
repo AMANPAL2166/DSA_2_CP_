a3420041fd418b139f677187ccba4c9e4cdb8f21
Aman Pal <apal60525@gmail.com>
2026-04-20 23:42:05 +0530

    "NEETCODE{LINKEDLIST}: Copy list random .

--- DSA_2_CP/src/NEETCODE/LINKED_LIST/CopyListRandom.java
+++ DSA_2_CP/src/NEETCODE/LINKED_LIST/CopyListRandom.java
@@ -33,6 +33,35 @@ public class CopyListRandom {
         return map.get(head);
 
     }
+//    Function copyRandomList(head):
+//    If head is null: Return null
+//
+//    // Step 1: Create interweaved list (A -> A' -> B -> B')
+//    curr = head
+//    While curr is not null:
+//    newNode = new Node(curr.val)
+//    newNode.next = curr.next
+//    curr.next = newNode
+//            curr = newNode.next
+//
+//    // Step 2: Set random pointers for copy nodes
+//    curr = head
+//    While curr is not null:
+//    If curr.random is not null:
+//    curr.next.random = curr.random.next
+//            curr = curr.next.next
+//
+//    // Step 3: Separate the two lists
+//    curr = head
+//            newHead = head.next
+//    While curr is not null:
+//    temp = curr.next
+//    curr.next = temp.next
+//    If temp.next is not null:
+//    temp.next = temp.next.next
+//            curr = curr.next
+//
+//    Return newHead
 
 }
 
