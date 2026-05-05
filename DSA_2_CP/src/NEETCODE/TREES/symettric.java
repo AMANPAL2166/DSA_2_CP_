db19b998f0ffcdaeed9a89ddd675fd32bdcdc6fb
AMANPAL2166 <apal60525@gmail.com>
2026-05-05 23:13:10 +0530

    NEETCODE{Tree}: Invert Binary Tree, Symmetric Tree, Right Side View

---
+++ DSA_2_CP/src/NEETCODE/TREES/symettric.java
@@ -0,0 +1,37 @@
+package NEETCODE.TREES;
+
+public class symettric {
+    public static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x){
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
+    
+    public boolean isSymmetric(TreeNode root){
+        if(root == null)return true;
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null ) return false;
+        //value should be same and arguments should be mirrors of each other
+        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        symettric s = new symettric();
+        System.out.println(s.isSymmetric(root));
+    }
+}
