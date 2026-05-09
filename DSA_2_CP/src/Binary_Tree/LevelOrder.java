fd684c405926b60322941e2d895bdfee853cd3e2
Aman Pal <apal60525@gmail.com>
2026-05-09 23:35:58 +0530

    "Binary Tree: Level order traversal.

--- DSA_2_CP/src/Binary_Tree/LevelOrder.java
+++ DSA_2_CP/src/Binary_Tree/LevelOrder.java
@@ -28,6 +28,44 @@ public class LevelOrder {
         traversal(root.left, level+1, res);
         traversal(root.right, level+1, res);
 
-
     }
+
+    public static void main(String[] args) {
+        //      5
+        //     / \
+        //   12   13
+        //   /  \    \
+        //  7    14   2
+        // / \  /  \  / \
+        //17 23 27 3  8  11
+
+        Node root = new Node(5);
+        root.left = new Node(12);
+        root.right = new Node(13);
+
+        root.left.left = new Node(7);
+        root.left.right = new Node(14);
+
+        root.right.right = new Node(2);
+
+        root.left.left.left = new Node(17);
+        root.left.left.right = new Node(23);
+
+        root.left.right.left = new Node(27);
+        root.left.right.right = new Node(3);
+
+        root.right.right.left = new Node(8);
+        root.right.right.right = new Node(11);
+
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        for (ArrayList<Integer> level : res) {
+            for (int val : level) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
