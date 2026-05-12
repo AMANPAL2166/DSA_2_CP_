3b290afe0fe168ccca2097bc6569535bd2053074
Aman Pal <apal60525@gmail.com>
2026-05-12 23:44:30 +0530

    "NEETCODE{Math}: Plus one problem.

--- DSA_2_CP/src/NEETCODE/MATH_and_GROMETRY/PlusOne.java
+++ DSA_2_CP/src/NEETCODE/MATH_and_GROMETRY/PlusOne.java
@@ -3,7 +3,7 @@ package NEETCODE.MATH_and_GROMETRY;
 public class PlusOne {
     public static int[] plusOne(int[] digits) {
         int n = digits.length;
-        for(int i = n-1;i>=0;i++){
+        for(int i = n-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
@@ -18,7 +18,7 @@ public class PlusOne {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
+        int[] arr = {1,9,9};
         plusOne(arr);
         for(int a: arr){
             System.out.print(a + " ");
