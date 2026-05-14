a667b57a8f4fdd45b363c496c1f8e7a075c53c9e
Aman Pal <apal60525@gmail.com>
2026-05-14 23:22:15 +0530

    "NEETCODE{Tree}: LevelOrder traversal.

--- DSA_2_CP/src/NEETCODE/TREES/LevelOrder.java
+++ DSA_2_CP/src/NEETCODE/TREES/LevelOrder.java
@@ -6,57 +6,85 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class LevelOrder {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+    static class  Node {
+        int data;
+        Node left, right;
+        Node(int value)
+        {
+            data = value;
+            left = null;
+            right = null;
         }
-    } 
-    public static void main(String[] args) {
-        // Creating nodes
-        TreeNode root = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        TreeNode n6 = new TreeNode(6);
-        TreeNode n7 = new TreeNode(7);
-
-        // Constructing tree
-        root.left = n2; // Node(2)
-        root.right = n3; // Node(3)
-
-        // Second level
-        n2.left = n4; // Node(4)
-        n2.right = n5; // Node(5)
-
-        //Third Level
-        n4.left = n6;
-        n4.right = n7;
     }
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>(); 
-            for(int i = 0; i<size; i++){
-                TreeNode node = queue.poll();
-                level.add(node.val);
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
+
+
+        void levelOrderRec(Node root, int level,
+                           ArrayList<ArrayList<Integer>> res)
+        {
+            // Base case
+            if (root == null)
+                return;
+
+            // Add a new level to the result if needed
+            if (res.size() <= level)
+                res.add(new ArrayList<>());
+
+            // Add current node's data to its corresponding
+            // level
+            res.get(level).add(root.data);
+
+            // Recur for left and right children
+            levelOrderRec(root.left, level + 1, res);
+            levelOrderRec(root.right, level + 1, res);
+        }
+
+        // Function to perform level order traversal
+        ArrayList<ArrayList<Integer>> levelOrder(Node root)
+        {
+
+            // Stores the result level by level
+            ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+            levelOrderRec(root, 0, res);
+            return res;
+        }
+        public static void main(String[] args)
+        {
+            //      5
+            //     / \
+            //   12   13
+            //   /  \    \
+            //  7    14   2
+            // / \  /  \  / \
+            //17 23 27 3  8  11
+
+            Node root = new Node(5);
+            root.left = new Node(12);
+            root.right = new Node(13);
+
+            root.left.left = new Node(7);
+            root.left.right = new Node(14);
+
+            root.right.right = new Node(2);
+
+            root.left.left.left = new Node(17);
+            root.left.left.right = new Node(23);
+
+            root.left.right.left = new Node(27);
+            root.left.right.right = new Node(3);
+
+            root.right.right.left = new Node(8);
+            root.right.right.right = new Node(11);
+
+            LevelOrder tree = new LevelOrder();
+            ArrayList<ArrayList<Integer>> res = tree.levelOrder(root);
+
+            for (ArrayList<Integer> level : res) {
+                for (int val : level) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
             }
-            ans.add(level);
         }
-        return ans;
     }
 
-}
+
