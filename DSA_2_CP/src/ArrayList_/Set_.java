9bd4f1bb16d2f56d5d76f3300ae2885a24875b13
Aman Pal <apal60525@gmail.com>
2026-05-18 23:54:21 +0530

    "Java collections: Set in java collection.

---
+++ DSA_2_CP/src/ArrayList_/Set_.java
@@ -0,0 +1,13 @@
+package ArrayList_;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set_ {
+    public static void main(String[] args) {
+//Unique elements, meaning it doesn't allow duplicates
+        //create a set using hashset
+        Set<Integer> s = new HashSet<>();
+        System.out.println(s);
+    }
+}
