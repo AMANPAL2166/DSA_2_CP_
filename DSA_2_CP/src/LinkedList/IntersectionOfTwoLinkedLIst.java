889344ebbdcdd538e34141c46093b5d4e5de394c
Aman Pal <apal60525@gmail.com>
2026-05-23 23:44:20 +0530

    "LinkedList": Intersection of two linkedlist.

---
+++ DSA_2_CP/src/LinkedList/IntersectionOfTwoLinkedLIst.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+public class IntersectionOfTwoLinkedLIst {
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int data){
+           val = data;
+           next = null;
+        }
+    }
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        if(headA == null|| headB == null)return null;
+       ListNode pA = headA;
+       ListNode pB = headB;
+       while(pA != pB){
+           pA = (pA == null) ? headB: pA.next;
+           pB  = (pB == null)? headA: pB.next;
+       }
+       return pA;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
