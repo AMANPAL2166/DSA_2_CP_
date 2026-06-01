f041b32ca10274a8c687bae8d101e014ac8e4c49
Aman Pal <apal60525@gmail.com>
2026-06-01 23:41:33 +0530

    "NEETCODE{BST}":Convert array to BST.

---
+++ DSA_2_CP/src/NEETCODE/TREES/BST/ConvertArrayToBST.java
@@ -0,0 +1,14 @@
+package NEETCODE.TREES.BST;
+
+public class ConvertArrayToBST {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val = x;
+            left = right = null;
+        }
+    }
+
+}
