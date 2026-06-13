a568ebf06ff4a6cd54ff86aa96e73fdeb36e2c84
Aman Pal <apal60525@gmail.com>
2026-06-13 10:40:34 +0530

    "ArrayList": Add two array and get sum.

---
+++ DSA_2_CP/src/ArrayList_/ADDtwoSUM.java
@@ -0,0 +1,14 @@
+package ArrayList_;
+
+import java.util.ArrayList;
+
+public class ADDtwoSUM {
+    public static ArrayList<Integer> twosum(int[] arr1, int[] arr2) {
+        ArrayList<Integer> ans= new ArrayList<>();
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Just checking !!");
+    }
+}
