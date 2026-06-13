3fb8fbf4513854a2d3b6b655c6f7ebbe063b124a
Aman Pal <apal60525@gmail.com>
2026-06-13 22:58:30 +0530

    "ArrayList": Add two array and get sum.

--- DSA_2_CP/src/ArrayList_/ADDtwoSUM.java
+++ DSA_2_CP/src/ArrayList_/ADDtwoSUM.java
@@ -5,10 +5,28 @@ import java.util.ArrayList;
 public class ADDtwoSUM {
     public static ArrayList<Integer> twosum(int[] arr1, int[] arr2) {
         ArrayList<Integer> ans= new ArrayList<>();
+        int maxLength;
+        if(arr1.length>arr2.length){
+            maxLength = arr1.length;
+        }else{
+            maxLength = arr2.length;
+        }
+        for(int i = maxLength;i>=0;i--){
+            boolean carray = false;
+            int sum = arr1[i] + arr2[i];
+//            if(sum)
+
+        }
         return ans;
     }
 
     public static void main(String[] args) {
         System.out.println("Just checking !!");
+        int[] arr1 = {1,2,3};
+        int[] arr2 = {1,2};
+        ArrayList<Integer> res = twosum(arr1,arr2);
+        for(int i : res){
+            System.out.println(i);
+        }
     }
 }
