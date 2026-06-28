342a5db28b287cd4bfc5e0ddbe9bed4f8125d6ba
Aman Pal <apal60525@gmail.com>
2026-06-28 23:25:50 +0530

    "String":Soved atoi problem.

--- DSA_2_CP/src/NEETCODE/RECURSION_AND_BACKTRACKING/Delete.java
+++ DSA_2_CP/src/NEETCODE/RECURSION_AND_BACKTRACKING/Delete.java
@@ -28,6 +28,37 @@ public class Delete {
         //Induction
         st.push(temp);
     }
+    public int myAtoi(String s) {
+        int n = s.length();
+        int num = 0;
+        int i = 0;
+        int sign = 1;
+        //skip spaces
+        while(i<n && s.charAt(i) == ' '){
+            i++;
+        }
+        //handle sign
+        if(i<n){
+            if(s.charAt(i) == '-'){
+                sign = -1;
+                i++;
+            }else if(s.charAt(i) == '+'){
+                sign = 1;
+                i++;
+            }
+        }
+
+        while( i< n && s.charAt(i) >='0' && s.charAt(i) <='9'){
+            int digit= s.charAt(i)- '0';
+            //overflow check
+            if(num > Integer.MAX_VALUE/10 || (num == Integer.MAX_VALUE /10 && digit > 7)){
+                return (sign == 1) ? Integer.MAX_VALUE: Integer.MIN_VALUE;
+            }
+            num = num*10+digit;
+            i++;
+        }
+        return num*sign;
+    }
 
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
