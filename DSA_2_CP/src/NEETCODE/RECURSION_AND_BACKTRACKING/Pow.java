9441437e7a921a4168e54db32336d8a4a34ae81a
Aman Pal <apal60525@gmail.com>
2026-07-13 23:09:27 +0530

    "NEETCODE{RECURSION}":Return pow of two numbers.

---
+++ DSA_2_CP/src/NEETCODE/RECURSION_AND_BACKTRACKING/Pow.java
@@ -0,0 +1,13 @@
+package NEETCODE.RECURSION_AND_BACKTRACKING;
+
+public class Pow {
+//    private double myPow(int x, int n){
+//        //base case
+//        if(n== 0){
+//            return ans;
+//        }
+//        double ans= x*x;
+//        return myPow(x, n-1);
+//    }
+
+}
