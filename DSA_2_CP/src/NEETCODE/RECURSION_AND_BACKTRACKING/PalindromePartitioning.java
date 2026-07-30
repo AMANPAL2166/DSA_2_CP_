4908ec63c6b771c2f08e9231c608e5bbf4486fd7
Aman Pal <apal60525@gmail.com>
2026-07-30 23:54:15 +0530

    "NEETCODE{RECURSION}":Solved Palindrome partitions using for-loop iteration and checkpalindrome function.

---
+++ DSA_2_CP/src/NEETCODE/RECURSION_AND_BACKTRACKING/PalindromePartitioning.java
@@ -0,0 +1,42 @@
+package NEETCODE.RECURSION_AND_BACKTRACKING;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        partitionFunctio(s,ans,new ArrayList<>(), 0);
+        return ans;
+
+    }
+
+    private void partitionFunctio(String s, List<List<String>> ans, List<String> currCom, int index) {
+        //base case
+        if(index == s.length()){
+            ans.add(new ArrayList<>(currCom));
+            return;
+        }
+        for(int end = index;end<s.length();end++){
+            String part = s.substring(index, end+1);
+            if(isPalindrom(part)){
+                currCom.add(part);
+                partitionFunctio(s, ans, currCom, end+1);
+                currCom.remove(currCom.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrom(String part) {
+        int start = 0;
+        int end = part.length()-1;
+        while (start<end){
+            if(part.charAt(start) != part.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
