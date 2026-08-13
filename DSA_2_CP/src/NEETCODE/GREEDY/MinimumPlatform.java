6f7c950e2576c6acc214d67010bcedabb69a2aa4
Aman Pal <apal60525@gmail.com>
2026-08-13 23:09:23 +0530

    "NEETCODE{TREE}":Minimum platform required to handle trains in station.

--- DSA_2_CP/src/NEETCODE/GREEDY/MinimumPlatform.java
+++ DSA_2_CP/src/NEETCODE/GREEDY/MinimumPlatform.java
@@ -1,5 +1,7 @@
 package NEETCODE.GREEDY;
 
+import java.util.Arrays;
+
 public class MinimumPlatform {
     public static int countPlatform(int[] arriv, int[] dep, int n) {
         int ans = 1;
@@ -19,11 +21,32 @@ public class MinimumPlatform {
         }
         return ans;
     }
+    public static int minPlatform(int arr[], int dep[]) {
+        //  code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int platforms = 0;
+        int maxPlatform = 0;
+        int i = 0;
+        int j = 0;
+        while(i<arr.length){
+            if(arr[i] <= dep[j]){
+                platforms++;
+                i++;
+            }else{
+                platforms--;
+                j++;
+            }
+            maxPlatform = Math.max(maxPlatform, platforms);
+        }
+
+        return maxPlatform;
+    }
     public static void main(String[] args) {
         int[] arr = {900, 945, 955, 1100, 1500, 1800};
         int[] dep = {920, 1200, 1130, 1150, 1900, 2000};
         int n = arr.length;
         System.out.println("Minimum number of Platforms required " +
-                countPlatform(arr, dep,n));
+                minPlatform(arr, dep));
     }
 }
