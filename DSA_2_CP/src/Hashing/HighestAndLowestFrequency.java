ae7d9093de7e4942eb2919a0846cfab7dd8f4109
Aman Pal <apal60525@gmail.com>
2026-08-18 23:42:40 +0530

    "HASHING":Highest and lowest frequency count.

--- DSA_2_CP/src/Hashing/HighestAndLowestFrequency.java
+++ DSA_2_CP/src/Hashing/HighestAndLowestFrequency.java
@@ -34,8 +34,46 @@ public class HighestAndLowestFrequency {
         System.out.println("Max frequency count is " + maxFreq) ;
     }
 
+    public static void countFreq(int[] arr, int n) {
+        //make a boolean array to count visited element
+        //make maxFreq, maxEle, minfreq, minEle variable
+        //traverse from left to right of the array
+        //check if they already visited skip that ele
+        //otherwise: run another loop and initialize scoper variable called count
+        //check if count > maxFreq then change maxFre to count and ele with arr[i], repeate same thing for minferq
+        boolean[] visited = new boolean[n];
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(visited[i]) {
+                continue;
+            }
+            int count = 1;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    visited[j] = true;//mark as counted
+                    count++;
+                }
+            }
+            if(maxFreq<count){
+                maxFreq = count;
+                maxEle = arr[i];
+
+            }
+            if(minFreq>count){
+                minEle = arr[i];
+                minFreq = count;
+
+            }
+        }
+        // Print final results
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+
+    }
+
     public static void main(String[] args) {
          int array[] = {10,5,10,15,10,5};
-        freq(array, array.length);
+        countFreq(array, array.length);
     }
 }
