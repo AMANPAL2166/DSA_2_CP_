c49f5a707d9a3ff47a988c1c36c52035f222a03b
Aman Pal <apal60525@gmail.com>
2026-09-11 23:51:56 +0530

    feat(Tree): Construct binary tree preorder and inorder traversal.
    -Extract root from preorder and find its position in inorder array.
    -Divide inorder array into left and right subtree to build a tree structure.

--- DSA_2_CP/src/Binary_Tree/ConstructBTFromPreOrder.java
+++ DSA_2_CP/src/Binary_Tree/ConstructBTFromPreOrder.java
@@ -1,4 +1,35 @@
 package Binary_Tree;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConstructBTFromPreOrder {
+    public TreeNode built(int[] preOrder, int[] inOrder){
+        ///  Map to store value-> index from inorder
+        Map<Integer, Integer> inMap= new HashMap<>() ;
+        for(int i = 0;i<inOrder.length;i++){
+            inMap.put(inOrder[i], i);
+
+        }
+        //Start a recursive construction
+        return buildTree(preOrder, 0, preOrder.length-1, inOrder, 0, inOrder.length-1, inMap);
+
+    }
+
+    private TreeNode buildTree(int[] preOrder, int preStart, int preEnd, int[] inOrder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        //Base condition
+        if(preStart>preEnd ||inStart > inEnd){
+            return null;
+        }
+        /// First element of the preOrder is the root
+        TreeNode root = new TreeNode(preOrder[preStart]);
+        // Get inorder index of root
+        int inRoot = inMap.get(root.data);
+        int numsLeft = inRoot - inStart;
+
+        //Build left and right subtree
+        root.left = buildTree(preOrder, preStart+1, preStart + numsLeft, inOrder, inStart,inRoot-1, inMap);
+        root.right = buildTree(preOrder, preStart + numsLeft+1,preEnd, inOrder,inRoot + 1, inEnd,inMap);
+        return root;
+
+    }
 }
