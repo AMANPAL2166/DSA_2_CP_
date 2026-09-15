0e9e08c598e8cee067eb44c7267596799512cb87
Aman Pal <apal60525@gmail.com>
2026-09-15 23:31:12 +0530

    feat(tree): make TreeNode class public
    Exposes TreeNode publicly so it can be accessed across the entire directory structure.

--- DSA_2_CP/src/Binary_Tree/TreeNode.java
+++ DSA_2_CP/src/Binary_Tree/TreeNode.java
@@ -1,9 +1,10 @@
 package Binary_Tree;
 
 public class TreeNode {
-    TreeNode left, right;
-    int data;
-    TreeNode(int val) {
+    public TreeNode left;
+    public TreeNode right;
+    public int data;
+    public TreeNode(int val) {
         data = val;
         left = right = null;
     }
